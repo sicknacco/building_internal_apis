df1bdf60f531b0f5b931f0a2623ed27f95e87ab6
Nick Sacco <nicksacco10@gmail.com>
2023-06-26 16:01:40 -0700

    added books index route under namespaces

--- spec/factories/books.rb
+++ spec/factories/books.rb
@@ -1,9 +1,9 @@
 FactoryBot.define do
   factory :book do
-    title { "MyString" }
-    author { "MyString" }
-    genre { "MyString" }
-    summary { "MyText" }
-    number_sold { 1 }
+    title { Faker::Book.title }
+    author { Faker::Book.author }
+    genre { Faker::Book.genre }
+    summary { Faker::Lorem.paragraph }
+    number_sold { Faker::Number.within(range: 1..1000) }
   end
 end
