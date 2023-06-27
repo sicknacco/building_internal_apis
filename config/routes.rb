b19015978c3513efc3abb1c7f5b6dfdec6df072a
Nick Sacco <nicksacco10@gmail.com>
2023-06-26 17:46:25 -0700

    feat: delete action created

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      resources :books, only: [:index, :show, :create, :update]
+      resources :books
     end
   end
 end
