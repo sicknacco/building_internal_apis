7ba3656a755d07cd271080adafa00a3916fe1a76
Nick Sacco <nicksacco10@gmail.com>
2023-06-26 18:02:51 -0700

    feat: new v2 routes and controller with index action

---
+++ app/controllers/api/v2/books_controller.rb
@@ -0,0 +1,5 @@
+class Api::V2::BooksController < ApplicationController
+  def index
+    
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,9 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :books
     end
+
+    namespace :v2 do
+      resources :books, only: [:index]
+    end
   end
 end
